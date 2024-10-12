d3ff206ed3e60c83e3f8a1dec483827d85d418c9
Dhanush CK <dhanushck01@gmail.com>
2024-10-12 23:14:08 +0530

    feat : Set conditions

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,18 @@
 const options = document.querySelectorAll('.options');
+const computerImage = document.getElementById('computer-image');
+const computerResponse = document.getElementById('computer-response');
 const userImage = document.getElementById('user-image')
-const  userResponse = document.getElementById('user-response');
+const userResponse = document.getElementById('user-response');
+const result = document.querySelector('.result');
+let selection;
+
+function computerResult() {
+    selection = Math.floor(Math.random() * (options.length));
+    computerImage.src = options[selection].src;
+    const computerParentClass = computerImage.parentElement;
+    computerParentClass.style.display = 'block';
+    computerResponse.innerText = options[selection].alt;
+}
 
 options.forEach(option=>{
     option.onclick = ()=>{
@@ -8,5 +20,22 @@ options.forEach(option=>{
         const parentClass = userImage.parentElement;
         parentClass.style.display = 'block';
         userImage.src = option.src;
+        computerResult();
+        resultCheck();
     }
 })
+
+
+function resultCheck() {
+    const computerResponseText = computerResponse.innerText;
+    const userResponseText = userResponse.innerText;
+    if(computerResponseText == userResponseText){
+        result.innerText = "Draw";
+    }
+    else if(computerResponseText=="Stone"&&userResponseText=="Paper"){
+        result.innerText = "You Won";
+    }
+    else if(computerResponseText=="Paper"&&userResponseText=="Stone"){
+        result.innerText = "Computer Wins"; 
+    }
+}
